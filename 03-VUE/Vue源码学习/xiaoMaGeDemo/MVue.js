fc385a36628b32ba4828fd367a4e99d0d5f98848
chuhx <hx.chu@clustar.ai>
2020-09-28 16:46:50 +0800

    =

--- 03-VUE/Vue源码学习/xiaoMaGeDemo/MVue.js
+++ 03-VUE/Vue源码学习/xiaoMaGeDemo/MVue.js
@@ -1,4 +1,5 @@
 // 处理元素/处理文本/处理事件... (策略模式 值得借鉴)
+// 编译模板工具类
 const compileUtil = {
     // 获取值的方法
     getVal(expr, vm) {
@@ -6,28 +7,58 @@ const compileUtil = {
             return data[currentVal]
         }, vm.$data)
     },
-    getAttrs(expr,vm){
-
+    //设置值
+    setVal(vm,expr,val){
+        return expr.split('.').reduce((data, currentVal, index, arr) => {
+            return data[currentVal] = val
+        }, vm.$data)
+    },
+    //获取新值 对{{a}}--{{b}} 这种格式进行处理
+    getContentVal(expr, vm) {
+        return expr.replace(/\{\{(.+?)\}\}/g, (...args) => {
+            return this.getVal(args[1], vm);
+        })
     },
     text(node, expr, vm) { //expr 可能是 {{obj.name}}--{{obj.age}} 
         let val;
         if (expr.indexOf('{{') !== -1) {
             // 
             val = expr.replace(/\{\{(.+?)\}\}/g, (...args) => {
+                //绑定watcher从而更新视图
+                new Watcher(vm,args[1],()=>{           
+                    this.updater.textUpdater(node,this.getContentVal(expr, vm));
+                })
                 return this.getVal(args[1], vm);
             })
         }else{ //也可能是v-text='obj.name' v-text='msg'
             val = this.getVal(expr,vm);
         }
         this.updater.textUpdater(node, val);
+
     },
     html(node, expr, vm) {
         // html处理 非常简单 直接取值 然后调用更新函数即可
         let val = this.getVal(expr,vm);
+        // 订阅数据变化时 绑定watcher,从而更新函数
+        new Watcher(vm,expr,(newVal)=>{
+            this.updater.htmlUpdater(node, newVal);
+        })
         this.updater.htmlUpdater(node,val);
     },
     model(node, expr, vm) {
         const val = this.getVal(expr,vm);
+        // 订阅数据变化时 绑定更新函数 更新视图的变化
+
+        // 数据==>视图
+        new Watcher(vm, expr, (newVal) => {
+            this.updater.modelUpdater(node, newVal);
+        })
+        // 视图==>数据
+        node.addEventListener('input',(e)=>{
+            // 设置值
+            this.setVal(vm,expr,e.target.value);
+
+        },false);
         this.updater.modelUpdater(node,val);
     },
     // 对事件进行处理
@@ -37,7 +68,7 @@ const compileUtil = {
         // 添加事件 因为我们使用vue时 都不需要关心this的指向问题,这是因为源码的内部帮咱们处理了this的指向
         node.addEventListener(eventName,fn.bind(vm),false);
     },
-    // 绑定属性 简单的属性 已经处理 类名样式的绑定有点复杂 因为对应的值可能是对象 也可能是数组 
+    // 绑定属性 简单的属性 已经处理 类名样式的绑定有点复杂 因为对应的值可能是对象 也可能是数组 大家根据个人能力尝试写一下
     bind(node,expr,vm,attrName){
         let attrVal = this.getVal(expr,vm);
         this.updater.attrUpdater(node,attrName,attrVal);
@@ -50,9 +81,7 @@ const compileUtil = {
             node.value = value;
         },
         textUpdater(node, value) {
-
             node.textContent = value;
-
         },
         htmlUpdater(node,value){
             node.innerHTML = value;
@@ -60,6 +89,7 @@ const compileUtil = {
     }
 
 }
+
 
 
 // 编译数据的类
@@ -172,6 +202,21 @@ class MVue {
             new Observer(this.$data)
             // 实现一个指令解析器
             new Compile(this.$el, this)
+            // 把数据获取操作 vm上的取值操作 都代理到vm.$data上
+            this.proxyData(this.$data);
         }
     }
+     // 做个代理
+     proxyData(data){
+        for (const key in data) {
+           Object.defineProperty(this,key,{
+               get(){
+                   return data[key];
+               },
+               set(newVal){
+                   data[key] = newVal;
+               }
+           })
+        }
+     }
 }
--- 03-VUE/Vue源码学习/xiaoMaGeDemo/Observer.js
+++ 03-VUE/Vue源码学习/xiaoMaGeDemo/Observer.js
@@ -1,3 +1,45 @@
+// 添加订阅者 定义通知的方法
+class Dep{
+    constructor() {
+        this.subs = []
+    }
+    // 添加订阅者 也就是收集 watcher
+    addSub(watcher){
+        this.subs.push(watcher);
+    }
+    // 通知变化
+    notify(){
+        // 观察者中有个update方法 来更新视图
+        this.subs.forEach(w=>w.update());
+    }
+}
+
+// 添加 watcher 
+
+class Watcher{
+    constructor(vm,expr,cb) {
+        // 观察新值和旧值的变化,如果有变化 更新视图
+        this.vm = vm;
+        this.expr = expr;
+        this.cb = cb;
+        // 先把旧值存起来  
+        this.oldVal = this.getOldVal();
+    }
+    getOldVal(){
+        Dep.target = this;
+        let oldVal = compileUtil.getVal(this.expr,this.vm);
+        Dep.target = null;
+        return oldVal;
+    }
+    update(){
+        // 更新操作 数据变化后 Dep会发生通知 告诉观察者更新视图
+        let newVal = compileUtil.getVal(this.expr, this.vm);
+        if(newVal !== this.oldVal){
+            this.cb(newVal);
+        }
+    }
+}
+
 // 创建一个数据监听者  劫持并监听所有数据的变化
 class Observer{
     constructor(data) {
@@ -16,8 +58,10 @@ class Observer{
     defineReactive(obj,key,value){
         // 循环递归 对所有层的数据进行观察
         this.observe(value);//这样obj也能被观察了
+        const dep = new Dep();
         Object.defineProperty(obj,key,{
             get(){
+                Dep.target && dep.addSub(Dep.target);
                 return value;
             },
             set:(newVal)=>{
