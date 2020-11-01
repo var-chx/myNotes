0aa9ebb253882c1f3ff21e332e0a970a43217455
chuwx <var_chx@126.com>
2020-11-01 19:46:19 +0800

    -

--- 03-VUE/VUE 3.0/vue3-project/src/views/About.vue
+++ 03-VUE/VUE 3.0/vue3-project/src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page{{ aa }}</h1>
+    <h1>This is an about page{{ aa }}</h1>
   </div>
 </template>
+<script>
+import { ref } from 'vue'
+export default {
+  setup () {
+    console.log(123)
+    console.log(123)
+    let aa = ref(100)
+    aa.value++ 
+    return {
+      aa,
+    }
+  }
+}
+</script>
