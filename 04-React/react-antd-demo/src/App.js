125fb2c219a8104b2351b477ba826ec3ead113f5
chuhx <hx.chu@clustar.ai>
2020-11-06 16:15:42 +0800

    项目使用 Antd

--- 04-React/react-antd-demo/src/App.js
+++ 04-React/react-antd-demo/src/App.js
@@ -1,7 +1,19 @@
+import { Button, Row, Col } from 'antd'
 function App() {
   return (
     <div className="App">
-      hello world
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
+        <Row>
+            <Col span={24}>col</Col>
+        </Row>
+        <Row>
+            <Col span={12}>col-12</Col>
+            <Col span={12}>col-12</Col>
+        </Row>
     </div>
   );
 }
