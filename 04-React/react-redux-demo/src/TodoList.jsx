3477a94fe9d48983b90aa233fa41b7e4f28e68cf
chuhx <hx.chu@clustar.ai>
2020-11-09 17:41:42 +0800

    项目使用 Antd

--- 04-React/react-redux-demo/src/TodoList.jsx
+++ 04-React/react-redux-demo/src/TodoList.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Input, Button, List } from 'antd'
 export default class TodoList extends React.Component {
+    
     render () {
+        const listData = [
+            '早上8点开会',
+            '9点需求讨论',
+            '晚上5点对接 代码 review'
+        ]
         return (
-            <div>hello world</div>
+            <>
+                <div style={{ padding: '30px 20px'}}>
+                    <Input placeholder="Basic usage" style={{ width: '200px', marginRight: '10px'}} />
+                    <Button type="primary">增加</Button>
+                </div>
+                <div>
+                    <List
+                        header={<div>Header</div>}
+                        footer={<div>Footer</div>}
+                        bordered
+                        dataSource={listData}
+                        renderItem={item => (
+                            <List.Item>
+                            {item}
+                            </List.Item>
+                        )}
+                    />
+                </div>
+            </>
         )
     }
 }
