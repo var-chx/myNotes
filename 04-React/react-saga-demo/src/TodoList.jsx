3e54fc70a39fed0793cd88dcf714ba5764bbb568
chuhx <hx.chu@clustar.ai>
2020-11-10 10:51:00 +0800

    添加 list 组件

--- 04-React/react-saga-demo/src/TodoList.jsx
+++ 04-React/react-saga-demo/src/TodoList.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
-import { Button } from 'antd'
+import { Input, Button, List } from 'antd'
 export default class TodoList extends React.Component {
+    state = {
+        inputVal : '年后',
+        listData: [
+            '哈哈镜',
+            '我是你好'
+        ]
+    }
+    inputChangeHandle = e => {
+        alert(e.target.value)
+        this.setState({
+            inputVal: e.target.value
+        })
+    }
     render () {
         return (
-            <div>
+            <div style={{padding: '10px'}}>
+                <Input 
+                    style={{ width: '300px', margin: ' 0 20px 20px 0'}}
+                    onChange={this.inputChangeHandle}
+                />
                 <Button type="primary">Button</Button>
+                <List
+                    header={<div>Header</div>}
+                    footer={<div>Footer</div>}
+                    bordered
+                    dataSource={this.state.listData}
+                    renderItem={item => (
+                        <List.Item>
+                         {item}
+                        </List.Item>
+                    )}
+                />
             </div>
         )
     }
