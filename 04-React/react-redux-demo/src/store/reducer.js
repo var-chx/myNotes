6d7312704b458e9e5c18d10d0089f018565e368e
chuhx <hx.chu@clustar.ai>
2020-11-10 16:20:52 +0800

    优化reducer

--- 04-React/react-redux-demo/src/store/reducer.js
+++ 04-React/react-redux-demo/src/store/reducer.js
@@ -4,24 +4,41 @@ const defaultState = {
     listData: []
 }
 const reducer = (state = defaultState, action ) => {
-    console.log(state, action, 9090)
-    if (action.type === CHANGE_INPUT) {
-        let newState = JSON.parse(JSON.stringify(state))
-        newState.inputVal = action.value
-        return newState
-    } else if (action.type === ADD_ITEM) {
-        let newState = JSON.parse(JSON.stringify(state))
-        newState.listData.push(state.inputVal)
-        return newState
-    } else if (action.type === DEL_ITEM) {
-        let newState = JSON.parse(JSON.stringify(state))
-        newState.listData.splice(action.value, 1)
-        return newState
-    } else if (action.type === GET_LIST) {
-        let newState = JSON.parse(JSON.stringify(state))
-        newState.listData = action.value
-        return newState
+    // if (action.type === CHANGE_INPUT) {
+    //     let newState = JSON.parse(JSON.stringify(state))
+    //     newState.inputVal = action.value
+    //     return newState
+    // } else if (action.type === ADD_ITEM) {
+    //     let newState = JSON.parse(JSON.stringify(state))
+    //     newState.listData.push(state.inputVal)
+    //     return newState
+    // } else if (action.type === DEL_ITEM) {
+    //     let newState = JSON.parse(JSON.stringify(state))
+    //     newState.listData.splice(action.value, 1)
+    //     return newState
+    // } else if (action.type === GET_LIST) {
+    //     let newState = JSON.parse(JSON.stringify(state))
+    //     newState.listData = action.value
+    //     return newState
+    // }
+    // return state
+    const { type, value } = action
+    const newState = JSON.parse(JSON.stringify(state))
+    switch (type) {
+        case CHANGE_INPUT:
+            newState.inputVal = value
+            return newState
+        case ADD_ITEM:
+            newState.listData.push(state.inputVal)
+            return newState
+        case DEL_ITEM:
+            newState.listData.splice(value, 1)
+            return newState
+        case GET_LIST:
+            newState.listData = value
+            return newState
+        default:
+            return state
     }
-    return state
 }
 export default reducer
