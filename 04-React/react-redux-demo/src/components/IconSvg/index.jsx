edfdf86a47a24b44187f402378a004d8c8eaa90a
chuhx <hx.chu@clustar.ai>
2020-11-12 18:51:41 +0800

    fix: warning

--- 04-React/react-redux-demo/src/components/IconSvg/index.jsx
+++ 04-React/react-redux-demo/src/components/IconSvg/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function FnComponent (props) {
     return (
         <div>
-            <svg class="icon" aria-hidden="true">
+            <svg className="icon" aria-hidden="true">
                 <use xlinkHref={`#${props.type}`}></use>
             </svg>
         </div>
